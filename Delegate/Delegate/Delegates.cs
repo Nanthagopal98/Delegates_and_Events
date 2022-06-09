6b603e1989bdcbbb63f094e7d68fca860e7dca72
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-06-09 13:49:06 +0530

    [Nantha] - Delegates Added

---
+++ Delegate/Delegate/Delegates.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public delegate int dele(int x, int y);
+    public class Delegates
+    {       
+        public int NonStatic(int a, int b)
+        {
+            int c = a - b;
+            Console.WriteLine("using Non Static Addition " + c);
+            return c;
+        }
+        public static int StaticMethod(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine("using Non Static Addition " + c);
+            return c;
+        }
+        public static int add(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine("using Non Static Addition " + c);
+            return c;
+        }
+
+        public static void Main(string[] args)
+        {
+            dele delobj = Delegates.StaticMethod;            
+            delobj(10, 20);
+            delobj(50, 20);
+            Delegates de = new Delegates();
+            delobj = de.NonStatic;
+            delobj(100, 50);
+        }
+    }
+    
+}
